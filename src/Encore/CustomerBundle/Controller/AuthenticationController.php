5020071af6f0bfb36ae773d494082c62880e07a7
Ryne Cheow <rynecheow@gmail.com>
2013-11-04 00:02:28 +0800

    [update] sign up page done, with complete profile

--- src/Encore/CustomerBundle/Controller/AuthenticationController.php
+++ src/Encore/CustomerBundle/Controller/AuthenticationController.php
@@ -2,10 +2,13 @@
 
 namespace Encore\CustomerBundle\Controller;
 
+use Encore\CustomerBundle\Entity\Customer;
+use Encore\CustomerBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class AuthenticationController extends BaseController
 {
@@ -24,39 +27,147 @@ class AuthenticationController extends BaseController
         if ($request->getMethod() === "POST") {
             $form->handleRequest($request);
             if ($form->isValid()) {
-                die($request->request->get('email'));
+                $params = $form->getData();
+                $invalid = $this->validateSignupParams($params);
+                if ($invalid) {
+                    var_dump($invalid);
+                }
+                $this->createUser($params);
+
+                return $this->redirect($this->generateUrl("encore_complete_profile"));
             }
         }
-//        $request = $this->getRequest();
-//        if ($request->getMethod() === "POST") {
-//            // signup process
-//            if ( $request->request->get( 'signup-data' ) ) {
-//                $params = $request->request->get( 'signup-data' );
-//                $invalid = $this->validateSignupParams($params);
-//                if($invalid)
-//                {
-//                    return $invalid;
-//                }
-//                $success = $this->createUser($params);
-//
-//                if($success['status'] == 'success')
-//                {
-//                    if(!$success['message'])
-//                    {
-//                        return array('status'=>'success','responseData'=>$success['data']);
-//                    }
-//                }
-//                return $success;
-//            }
-//            // validateEmail
-//            elseif ($request->request->get('email')) {
-//                return $this->validateEmail( $request->request->get('email') );
-//            }
-//        }
-//        return [];
-
 
         return $this->render("EncoreCustomerBundle:Authentication:signup.html.twig", ["form" => $form->createView()]);
+    }
+
+    /**
+     * User which is not enabled complete information
+     * @Route("/signup/complete-profile", name="encore_complete_profile")
+     */
+    public function completeInformationAction(Request $request)
+    {
+        // Only display form if user is logged in and not completed profile
+        if ($this->isLoggedIn()) {
+            $user = $this->authenticatedUser;
+            if (!($user->isEnabled())) {
+
+                $form = $this->createFormBuilder()
+                    ->setAction('encore_complete_profile')
+                    // Get First Email In Email Array
+                    ->add(
+                        'email',
+                        'email',
+                        [
+                            // HTML Attributes
+                            'attr' => [
+                                'value' => $user->getEmails()[0]->getEmail(),
+                                'disabled',
+                                ''
+                            ],
+                            'label' => 'Email :'
+                        ]
+                    )
+                    ->add('firstName', 'text', ['label' => 'First Name :'])
+                    ->add('lastName', 'text', ['label' => 'Last Name :'])
+                    ->add('birthDate', 'birthday', ['label' => 'Birth Date :'])
+                    ->add('contactNo', 'number', ['label' => 'Contact No :'])
+                    ->add('address', 'textarea', ['label' => 'Address :'])
+                    ->add('edit', 'submit')
+                    ->getForm();
+
+                $form->handleRequest($request);
+                if ($form->isValid()) {
+                    $data = $form->getData();
+                    $this->completeInformation($user, $data);
+                }
+
+                return $this->render(
+                    "EncoreCustomerBundle:User:profile.html.twig",
+                    ['form' => $form->createView()]
+                );
+            }
+
+            // If Enabled
+            return $this->redirect($this->generateUrl('encore_home'));
+        }
+
+        // Invalid Access via url
+        return $this->redirect($this->generateUrl('fos_user_security_login'));
+
+    }
+
+    private function completeInformation($user, $data)
+    {
+        if ($this->isLoggedIn()) {
+            $request = $this->getRequest();
+
+            if (($request->getMethod() === "POST") && ($request->request->get("encore_complete_profile"))) {
+                $validate = $this->validateCustomerInfo($data);
+
+                if ($validate["status"]) {
+                    $success = $this->createCustomer($user, $data);
+
+                    if ($success["status"]) {
+                        // Redirect To Home Page
+                        return $this->generateUrl("encore_customer_welcome");
+                    }
+
+                }
+            }
+        } else {
+            return $this->render("EncoreCustomerBundle:Security:login.html.twig");
+        }
+    }
+
+    /**
+     * @param $user
+     * @param $params
+     *
+     * @return array
+     */
+    private function createCustomer($user, $params)
+    {
+        $customer = new Customer();
+        $customer->setUser($user)
+            ->setFirstName($params["firstName"])
+            ->setLastName($params["lastName"])
+            ->setBirthDate($params["birthDate"])
+            ->setContactNo($params["contactNo"])
+            ->setAddress($params["address"]);
+
+        $this->em->persist($customer);
+        $this->em->flush();
+        $user->setEnabled(true);
+        $this->em->flush();
+
+        return ["status" => true, "customer" => $customer];
+    }
+
+    /**
+     * @param $params
+     *
+     * @return array
+     */
+    private function validateCustomerInfo($params)
+    {
+        $error = ["status" => true, "message" => ""];
+        $entities = array("firstName", "lastName", "birthDate", "contactNo", "address");
+        $count = count($entities);
+        $i = 0;
+
+        while ($i < $count) {
+            if (!isset($params[$entities[$i]])) {
+                $error["status"] = false;
+                $error["message"] = $entities[$i] . " is required.";
+
+                return $error;
+            }
+
+            $i++;
+        }
+
+        return $error;
     }
 
     /**
@@ -85,7 +196,7 @@ class AuthenticationController extends BaseController
     {
         $status = 'success';
         $msg = 'OK';
-        $email_exists = $this->em->getRepository('EncoreUserBundle:UserEmail')->findBy(array('email' => $email));
+        $email_exists = $this->em->getRepository('EncoreCustomerBundle:UserEmail')->findBy(array('email' => $email));
 
         if (count($email_exists)) {
             $status = 'error';
@@ -95,6 +206,38 @@ class AuthenticationController extends BaseController
         $result = array('status' => $status, 'message' => $msg);
 
         return $result;
+    }
+
+    /**
+     * @param $params
+     *
+     * @return array
+     */
+    private function validateSignupParams($params)
+    {
+        $error = [];
+
+        if (!isset($params['email']) || str_replace(" ", "", $params['email']) == "") {
+            $error['status'] = 'fail';
+            $error['message'] = 'Email is required.';
+        } else {
+            $email_check = $this->validateEmail($params['email']);
+            if ($email_check['status'] == 'error') {
+                $error = $email_check;
+            }
+        }
+
+        if (!isset($params['password']) || str_replace(" ", "", $params['password']) == "") {
+            $error['status'] = 'fail';
+            $error['message'] = 'Password is required.';
+        }
+
+//        if (!isset($params['username']) || str_replace(" ", "", $params['username']) == "") {
+//            $error['status'] = 'fail';
+//            $error['message'] = 'Username is required.';
+//        }
+
+        return $error;
     }
 
     /**
@@ -123,20 +266,20 @@ class AuthenticationController extends BaseController
                     'label' => false
                 ]
             )
-            ->add(
-                'username',
-                'text',
-                [
-                    'attr' => [
-                        'class' => 'signup-username',
-                        'placeholder' => 'Username',
-                        'data-required' => 'true',
-                        'data-trigger' => 'change',
-                        'data-required-message' => 'Please enter your username.',
-                    ],
-                    'label' => false
-                ]
-            )
+//            ->add(
+//                'username',
+//                'text',
+//                [
+//                    'attr' => [
+//                        'class' => 'signup-username',
+//                        'placeholder' => 'Username',
+//                        'data-required' => 'true',
+//                        'data-trigger' => 'change',
+//                        'data-required-message' => 'Please enter your username.',
+//                    ],
+//                    'label' => false
+//                ]
+//            )
             ->add(
                 'password',
                 'password',
@@ -197,5 +340,43 @@ class AuthenticationController extends BaseController
             )
 
             ->getForm();
+    }
+
+    private function createUser($params)
+    {
+//        $now = new \DateTime();
+        $userManager = $this->get('encore.user_manager');
+
+        $user = new User();
+        $user->setUsername($params['email']);
+        $user->setEmail($params['email']);
+        $array_role = array(
+            User::ROLE_USER,
+        );
+        $user->setRoles($array_role);
+//        $user->setSignedUpAt($now);
+        $user->setEnabled(false);
+        $fosUserManager = $this->get('fos_user.user_manager');
+        $user->setPlainPassword($params['password']);
+        $fosUserManager->updatePassword($user);
+
+        $user_email = $userManager->createNewEmail($user);
+        $user_email->setEmail($params['email']);
+
+        $this->em->persist($user);
+        $this->em->persist($user_email);
+        $this->em->flush();
+
+        $username = $user->getUserName() . '_' . $user->getId();
+
+        $user->setUsername($username);
+
+        $this->em->flush();
+
+        $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
+
+        $this->get('security.context')->setToken($token);
+
+        $this->get('session')->set('_security_customer', serialize($token));
     }
 } 
--- src/Encore/CustomerBundle/Controller/ProfileController.php
+++ src/Encore/CustomerBundle/Controller/ProfileController.php
@@ -11,52 +11,7 @@ use Encore\CustomerBundle\Entity\User;
 class ProfileController extends BaseController
 {
 
-    /**
-     * User which is not enabled complete information
-     * @Route("/signup/complete-profile", name="encore_complete_profile")
-     */
-    public function completeInformationAction(Request $request)
-    {
-        // Only display form if user is logged in and not completed profile
-        if ($this->isLoggedIn()) {
-            $user = $this->authenticatedUser;
-            if (!($user->isEnabled())) {
 
-                $form = $this->createFormBuilder()
-                    ->setAction('encore_complete_profile')
-                    // Get First Email In Email Array
-                    ->add('email', 'email' , array(
-                                            // HTML Attributes
-                                            'attr' => array('value'=> $user->getEmails()[0],
-                                                            'disabled', '' ),
-                                            'label' => 'Email :'
-                    ))
-                    ->add('firstName', 'text' , array('label' => 'First Name :'))
-                    ->add('lastName', 'text', array('label' => 'Last Name :'))
-                    ->add('birthDate', 'birthday', array('label' => 'Birth Date :'))
-                    ->add('contactNo', 'number', array('label' => 'Contact No :'))
-                    ->add('address', 'textarea', array('label' => 'Address :'))
-                    ->add('edit','submit')
-                    ->getForm();
-
-                $form->handleRequest($request);
-                if ($form->isValid()) {
-                    $data = $form->getData();
-                    $this->completeInfoAction($data);
-                }
-
-                return $this->render(
-                    "EncoreCustomerBundle:User:profile.html.twig",
-                    array('form' => $form->createView())
-                );
-            }
-            // If Enabled
-            return $this->redirect($this->generateUrl('encore_home'));
-        }
-        // Invalid Access via url
-        return $this->redirect($this->generateUrl('fos_user_security_login'));
-
-    }
 
     /**
      * Logged In User Editing Its Profile
@@ -124,75 +79,7 @@ class ProfileController extends BaseController
      * @param $data
      * @return Response
      */
-    private function completeInfoAction($data)
-    {
-        if ($this->isLoggedIn()) {
-            $request = $this->getRequest();
 
-            if (($request->getMethod() === "POST") && ($request->request->get("encore_complete_profile"))) {
-                $validate = $this->validateCustomerInfo($data);
 
-                if ($validate["status"]) {
-                    $success = $this->createCustomer($data);
 
-                    if ($success["status"]) {
-                        // Redirect To Home Page
-                        return $this->generateUrl("encore_customer_welcome");
-                    }
-
-                }
-            }
-        } else {
-            return $this->render("EncoreCustomerBundle:Security:login.html.twig");
-        }
-    }
-
-    /**
-     * @param $params
-     *
-     * @return array
-     */
-    private function createCustomer($params)
-    {
-        $user = $this->authenticatedUser;
-        $customer = new Customer();
-        $customer->setUser($user)
-            ->setFirstName($params["firstName"])
-            ->setLastName($params["lastName"])
-            ->setBirthDate($params["birthDate"])
-            ->setContactNo($params["contactNo"])
-            ->setAddress($params["address"]);
-
-        $this->em->persist($customer);
-        $this->em->flush();
-        $user->setEnabled(true);
-
-        return array("status" => true, "customer" => $customer);
-    }
-
-    /**
-     * @param $params
-     *
-     * @return array
-     */
-    private function validateCustomerInfo($params)
-    {
-        $error = ["status" => true, "message" => ""];
-        $entities = array("firstName", "lastName", "birthDate", "contactNo", "address");
-        $count = count($entities);
-        $i = 0;
-
-        while ($i < $count) {
-            if (!isset($params[$entities[$i]])) {
-                $error["status"] = false;
-                $error["message"] = $entities[$i] . " is required.";
-
-                return $error;
-            }
-
-            $i++;
-        }
-
-        return $error;
-    }
 } 
