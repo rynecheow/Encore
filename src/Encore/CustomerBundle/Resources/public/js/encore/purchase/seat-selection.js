6d0640245c80a7824b86a47b3cd9aff480669790
Bryan Lai <bryanlai@mystudentpartners.com>
2013-11-09 01:25:48 +0800

    [update] seat selection disallow user to unselect middle selected seats

--- src/Encore/CustomerBundle/Resources/public/js/encore/purchase/seat-selection.js
+++ src/Encore/CustomerBundle/Resources/public/js/encore/purchase/seat-selection.js
@@ -1,169 +1,3 @@
-///*global window */
-///*global $ */
-///*global alert */
-///*global require */
-//
-//require(['domReady'],
-//    function (domReady) {
-//        "use strict";
-//        domReady(
-//            function () {
-//                window.prevSelectedSeats = [];
-//                window.prevSelectedSec = -1;
-//                window.prevSelectedRow = -1;
-//
-//                function loadSection() {
-//                    var sectionArrays = [
-//                        {row: 10, col: 5},
-//                        {row: 8, col: 8},
-//                        {row: 6, col: 10}
-//                    ];
-//
-//                    return sectionArrays;
-//                }
-//
-//                function addSection(sectionIndex, section) {
-//                    /*jslint browser:true */
-//                    var body, tableTag, tableBody, row, col, currRow, tableRow, currCol, tableCol, tableDiv;
-//                    body = document.getElementsByTagName("body")[0];
-//                    tableTag = document.createElement("table");
-//                    tableTag.setAttribute("class", "section");
-//                    tableBody = document.createElement("tbody");
-//                    row = parseInt(section.row, 10);
-//                    col = parseInt(section.col, 10);
-//
-//                    currRow = 0;
-//                    while (currRow < row) {
-//
-//                        tableRow = document.createElement("tr");
-//
-//                        currCol = 0;
-//                        while (currCol < col) {
-//                            tableCol = document.createElement("td");
-//                            tableDiv = document.createElement("div");
-//
-//                            tableDiv.setAttribute("data-row", String(currRow));
-//                            tableDiv.setAttribute("data-col", String(currCol));
-//                            tableDiv.setAttribute("data-sec", String(sectionIndex));
-//                            tableDiv.setAttribute("class", "empty");
-//                            tableCol.appendChild(tableDiv);
-//                            tableRow.appendChild(tableCol);
-//
-//                            currCol = currCol + 1;
-//                        }
-//
-//                        tableBody.appendChild(tableRow);
-//                        currRow = currRow + 1;
-//                    }
-//
-//                    tableTag.appendChild(tableBody);
-//                    body.appendChild(tableTag);
-//                }
-//
-//                $(document).click(function (e) {
-//                    var selectedElement, col, row, table, mostLeftSeat, mostRightSeat, selectedSec, selectedRow, selectedCol, selectedFlag, index;
-//
-//                    if (!e) {
-//                        e = window.event;
-//                    }
-//
-//                    if (e.target) {
-//                        selectedElement = e.target;
-//                    } else if (e.srcElement) {
-//                        selectedElement = e.srcElement;
-//                    }
-//
-//                    if (selectedElement.tagName === "DIV") {
-//                        col = selectedElement.parentNode;
-//
-//                        if (col.tagName === "TD") {
-//                            row = col.parentNode;
-//
-//                            if (row.tagName === "TR") {
-//                                table = row.parentNode.parentNode;
-//
-//                                if (table.tagName === "TABLE" && table.classList.contains("section")) {
-//
-//                                    window.prevSelectedSeats.sort();
-//                                    mostLeftSeat = parseInt(window.prevSelectedSeats[0], 10);
-//                                    mostRightSeat = parseInt(window.prevSelectedSeats[window.prevSelectedSeats.length - 1], 10);
-//                                    selectedSec = parseInt(selectedElement.getAttribute("data-sec"), 10);
-//                                    selectedRow = parseInt(selectedElement.getAttribute(("data-row")), 10);
-//                                    selectedCol = parseInt(selectedElement.getAttribute("data-col"), 10);
-//                                    selectedFlag = true;
-//
-//                                    if (selectedElement.classList.contains("selected")) {
-//
-//                                        if (selectedCol === mostLeftSeat || selectedCol === mostRightSeat) {
-//                                            selectedFlag = true;
-//                                        } else {
-//                                            selectedFlag = false;
-//                                        }
-//
-//
-//                                        if (selectedFlag) {
-//                                            selectedElement.setAttribute("class", "empty");
-//                                            index = parseInt(window.prevSelectedSeats.indexOf(parseInt(selectedElement.getAttribute("data-col"), 10)), 10);
-//
-//                                            if (index !== -1) {
-//                                                window.prevSelectedSeats.splice(index, 1);
-//                                            }
-//                                        } else {
-//                                            alert("Cannot unselect seats in the middle");
-//                                        }
-//                                    } else {
-//
-//                                        if (window.prevSelectedSeats.length !== 0) {
-//                                            if (selectedSec === window.prevSelectedSec) {
-//                                                if (selectedRow === window.prevSelectedRow) {
-//                                                    if (selectedCol !== mostLeftSeat - 1 && selectedCol !== mostRightSeat + 1) {
-//                                                        selectedFlag = false;
-//                                                    }
-//                                                } else {
-//                                                    selectedFlag = false;
-//                                                }
-//                                            } else {
-//                                                selectedFlag = false;
-//                                            }
-//                                        }
-//
-//                                        if (selectedFlag) {
-//                                            selectedElement.setAttribute("class", "selected");
-//                                            window.prevSelectedSeats.push(selectedCol);
-//                                            window.prevSelectedSec = selectedSec;
-//                                            window.prevSelectedRow = selectedRow;
-//
-//                                        } else {
-//                                            alert("Must select adjacent seats");
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//                    );
-//
-//
-//                $("#resetButton").click(function (e) {
-//                    window.prevSelectedSeats = [];
-//                    window.prevSelectedRow = -1;
-//                    window.prevSelectedSec = -1;
-//
-//                    $("div.selected").each(function () {
-//                        this.setAttribute("class", "empty");
-//                    });
-//                });
-//
-//                $(function () {
-//                    var sections = loadSection();
-//                    $.each(sections, addSection);
-//                });
-//            }
-//        );
-//    }
-//    );
-
 require(['domReady'],
     function (domReady) {
         "use strict";
@@ -175,70 +9,10 @@ require(['domReady'],
                     row: 5
                 };
 
-                function detectEmptySeat(e) {
-
-                    var str = $(e.target).attr("id");
-                    var target = $(e.target);
-                    var endOfRow = str.indexOf("row");
-                    var endOfCol = str.indexOf("col") + 3;
-                    var colValue = parseInt(str.substring(endOfCol, str.length), 10);
-                    var rowValue = parseInt(str.substring(0, endOfRow), 10);
-                    console.log("row is " + rowValue);
-                    console.log("col is " + colValue);
-
-                    var totalCol = parseInt(seatsMatrix.col, 10);
-                    var previousCol = parseInt(colValue - 2, 10);
-                    var nextCol = parseInt(colValue + 2, 10);
-
-                    var middlePreviousCol = parseInt(colValue - 1, 10);
-                    var middleNextCol = parseInt(colValue + 1, 10);
-
-                    console.log("prev col  is " + previousCol);
-                    console.log("next col is " + nextCol);
-
-                    var previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
-                    var previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
-
-                    var nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
-                    var nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
-
-                    if (target.hasClass("empty")) {
-                        if (previousDiv.hasClass("empty")) {
-
-                            if (previousTwoDiv.hasClass("selected")) {
-                                alert("cannot!");
-                            }
-                            else {
-                                if (checkNext(nextDiv, nextTwoDiv)) {
-                                    tickDiv(e);
-                                }
-                                else {
-                                    alert("error");
-                                }
-
-                            }
-                        }
-                        else {
-                            if (checkNext(nextDiv, nextTwoDiv)) {
-                                tickDiv(e);
-                            }
-                            else {
-                                alert("error");
-                            }
-                        }
-                    }
-                    else {
-                        target.attr("class", "empty");
-                    }
-
-
-                }
-
                 function tickDiv(e) {
                     if ($(e.target).hasClass("empty")) {
                         $(e.target).attr("class", "selected");
-                    }
-                    else {
+                    } else {
                         $(e.target).attr("class", "empty");
                     }
                 }
@@ -247,46 +21,114 @@ require(['domReady'],
                     if (next1.hasClass("empty")) {
                         return !next2.hasClass("selected");
                     }
-                    else {
-                        return true;
-                    }
+                    return true;
                 }
 
-                function initSeatsPlan(matrix) {
-                    console.log(matrix.row);
-                    var i, j;
-                    var parent = $("body");
-                    var seatTable = $('<table/>', {
-                        "class": "seatTable",
-                        "id": "section1"
-                    }).appendTo(parent);
+                function detectEmptySeat(e) {
+                    var str, target, endOfRow, endOfCol, colValue, rowValue, totalCol, previousCol, nextCol, middlePreviousCol, middleNextCol, previousDiv, previousTwoDiv, nextDiv, nextTwoDiv;
+                    str = $(e.target).attr("id");
+                    target = $(e.target);
+                    endOfRow = str.indexOf("row");
+                    endOfCol = str.indexOf("col") + 3;
+                    colValue = parseInt(str.substring(endOfCol, str.length), 10);
+                    rowValue = parseInt(str.substring(0, endOfRow), 10);
 
-                    for (i = 0; i < matrix.row; i++) {
-                        var seatRow = $('<tr/>', {
-                            "id": "row" + i
-                        }).appendTo(seatTable);
+                    totalCol = parseInt(seatsMatrix.col, 10);
+                    previousCol = parseInt(colValue - 2, 10);
+                    nextCol = parseInt(colValue + 2, 10);
 
-                        for (j = 0; j < matrix.col; j++) {
-                            var seatColumn = $('<td/>').width(30).height(30).appendTo(seatRow);
+                    middlePreviousCol = parseInt(colValue - 1, 10);
+                    middleNextCol = parseInt(colValue + 1, 10);
 
-                            var emptyDiv = $('<div/>', {
-                                "id": i + "row" + "col" + j,
-                                "class": "empty",
-                                width: 20,
-                                height: 20,
-                                "position": "absolute"
-                            }).on("click", detectEmptySeat).appendTo(seatColumn);
+                    previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
+                    previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
+
+                    nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
+                    nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
+
+                    if (target.hasClass("empty")) {
+                        if (previousDiv.hasClass("empty")) {
+
+                            if (previousTwoDiv.hasClass("selected")) {
+                                alert("You cannot select a seat which leaves a single seat gap.");
+                            } else {
+                                if (checkNext(nextDiv, nextTwoDiv)) {
+                                    tickDiv(e);
+                                } else {
+                                    alert("You cannot select a seat which leaves a single seat gap.");
+                                }
+
+                            }
+                        } else {
+                            if (checkNext(nextDiv, nextTwoDiv)) {
+                                tickDiv(e);
+                            } else {
+                                alert("You cannot select a seat which leaves a single seat gap.");
+                            }
+                        }
+                    } else if (target.hasClass("selected")) {
+                        if (nextDiv.hasClass("empty") || previousDiv.hasClass("empty")) {
+                            target.attr("class", "empty");
+                        } else if (nextDiv.hasClass("selected") && !previousDiv.hasClass("selected")) {
+                            target.attr("class", "empty");
+                        } else if (previousDiv.hasClass("selected") && !nextDiv.hasClass("selected")) {
+                            target.attr("class", "empty");
+                        } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
+                            if (totalCol === middleNextCol) {
+                                target.attr("class", "empty");
+                            } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                                target.attr("class", "empty");
+                            } else {
+                                alert("You cannot un-select a seat which leaves a single seat gap.");
+                            }
+                        } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                            if (totalCol === middlePreviousCol) {
+                                target.attr("class", "empty");
+                            } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
+                                target.attr("class", "empty");
+                            } else {
+                                alert("You cannot un-select a seat which leaves a single seat gap.");
+                            }
+                        } else {
+                            alert("You cannot un-select a seat which leaves a single seat gap.");
                         }
                     }
                 }
 
+                function initSeatsPlan(matrix) {
+                    var i, j, parent, seatTable, seatRow, seatColumn;
+                    parent = $("body");
+                    seatTable = $('<table/>', {
+                        "class": "seatTable",
+                        "id": "section1"
+                    }).appendTo(parent);
+
+                    for (i = 0; i < matrix.row; i = i + 1) {
+                        seatRow = $('<tr/>', {
+                            "id": "row" + i
+                        }).appendTo(seatTable);
+
+                        for (j = 0; j < matrix.col; j = j + 1) {
+                            seatColumn = $('<td/>').appendTo(seatRow);
+
+                            $('<div/>', {
+                                "id": i + "row" + "col" + j,
+                                "class": "empty"
+                            }).on("click", detectEmptySeat).appendTo(seatColumn);
+                        }
+                    }
+                }
+                $("#resetButton").click(function () {
+                    $("table.seatTable tbody tr td div.selected").each(function () {
+                        $(this).attr("class", "empty");
+                    });
+                });
 
                 $(function () {
                     initSeatsPlan(seatsMatrix);
                 });
             }
 
-        )
-        ;
+        );
     }
-);
+    );
