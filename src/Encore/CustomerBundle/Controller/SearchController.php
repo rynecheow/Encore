67a99086f2f68859d751b58313c521ef45409887
Ryne Cheow <rynecheow@gmail.com>
2013-11-09 23:32:37 +0800

    [update] search function

--- src/Encore/CustomerBundle/Controller/SearchController.php
+++ src/Encore/CustomerBundle/Controller/SearchController.php
@@ -19,4 +19,33 @@ class SearchController extends BaseController{
     public function searchAction(){
         return $this->render("EncoreCustomerBundle:Search:advance-search.html.twig");
     }
+
+    /**
+     * @Route("/search", name="encore_global_search")
+     */
+    public function globalSearchAction()
+    {
+        $params = [];
+        $session = $this->get('session');
+        $session->save();
+        if ($this->getRequest()->getMethod() === "GET") {
+            $qparams = $this->getRequest()->query->all();
+            if ($qparams) {
+                if (isset($qparams['q']) && $qparams['q']) {
+                    if (!isset($qparams['type'])) {
+                        $qparams['type'] = "default";
+                    }
+                    $qparams['user_id'] = ($this->authenticatedUser) ? $this->authenticatedUser->getId() : 0;
+                    $search_results = $this->get('encore.encore_search')->doFullSearch($qparams);
+                    $params = [
+                        "search_results" => $search_results
+                    ];
+
+                    return $this->render('EncoreCustomerBundle:Search:search.html.twig', $params);
+                }
+            }
+        }
+
+        return $this->render('EncoreCustomerBundle:Search:empty-search.html.twig', $params);
+    }
 } 
