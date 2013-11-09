9ff86f609c8c25ad65d447a9730c0f1fff000e1f
Bryan Lai <bryanlai@mystudentpartners.com>
2013-11-09 15:23:27 +0800

    [update] seat selection disable user from submitting form with empty seat in between selected seats disable user to submit without selecting any seats seat plan using twig instead of javascript

--- src/Encore/CustomerBundle/Resources/public/js/encore/purchase/seat-selection.js
+++ src/Encore/CustomerBundle/Resources/public/js/encore/purchase/seat-selection.js
@@ -9,11 +9,10 @@ require(['domReady'],
                     row: 5
                 };
 
-                function tickDiv(e) {
-                    if ($(e.target).hasClass("empty")) {
-                        $(e.target).attr("class", "selected");
+                function tickDiv(target) {
+                    if (target.hasClass("empty")) {
+                        target.attr("class", "selected");
                     } else {
-                        $(e.target).attr("class", "empty");
                     }
                 }
 
@@ -24,108 +23,211 @@ require(['domReady'],
                     return true;
                 }
 
-                function detectEmptySeat(e) {
-                    var str, target, endOfRow, endOfCol, colValue, rowValue, totalCol, previousCol, nextCol, middlePreviousCol, middleNextCol, previousDiv, previousTwoDiv, nextDiv, nextTwoDiv;
-                    str = $(e.target).attr("id");
-                    target = $(e.target);
-                    endOfRow = str.indexOf("row");
-                    endOfCol = str.indexOf("col") + 3;
-                    colValue = parseInt(str.substring(endOfCol, str.length), 10);
-                    rowValue = parseInt(str.substring(0, endOfRow), 10);
+                function checkSeatPlan(target) {
+                    var str, endOfRow, endOfCol, colValue, rowValue, totalCol, previousCol, nextCol, middlePreviousCol, middleNextCol, previousDiv, previousTwoDiv, nextDiv, nextTwoDiv;
+                    if (target.is("table.seatTable tbody tr td div")) {
+                        str = target.attr("id");
+                        endOfRow = str.indexOf("row");
+                        endOfCol = str.indexOf("col") + 3;
+                        colValue = parseInt(str.substring(endOfCol, str.length), 10);
+                        rowValue = parseInt(str.substring(0, endOfRow), 10);
 
-                    totalCol = parseInt(seatsMatrix.col, 10);
-                    previousCol = parseInt(colValue - 2, 10);
-                    nextCol = parseInt(colValue + 2, 10);
+                        totalCol = parseInt(seatsMatrix.col, 10);
+                        previousCol = parseInt(colValue - 2, 10);
+                        nextCol = parseInt(colValue + 2, 10);
 
-                    middlePreviousCol = parseInt(colValue - 1, 10);
-                    middleNextCol = parseInt(colValue + 1, 10);
+                        middlePreviousCol = parseInt(colValue - 1, 10);
+                        middleNextCol = parseInt(colValue + 1, 10);
 
-                    previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
-                    previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
+                        previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
+                        previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
 
-                    nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
-                    nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
+                        nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
+                        nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
 
-                    if (target.hasClass("empty")) {
-                        if (previousDiv.hasClass("empty")) {
+                        if (target.hasClass("empty")) {
+                            if (previousDiv.hasClass("empty")) {
 
-                            if (previousTwoDiv.hasClass("selected")) {
-                                alert("You cannot select a seat which leaves a single seat gap.");
+                                if (previousTwoDiv.hasClass("selected")) {
+                                    alert("You cannot select a seat which leaves a single seat gap.");
+                                } else {
+                                    if (checkNext(nextDiv, nextTwoDiv)) {
+                                        tickDiv(target);
+                                    } else {
+                                        alert("You cannot select a seat which leaves a single seat gap.");
+                                    }
+                                }
                             } else {
                                 if (checkNext(nextDiv, nextTwoDiv)) {
-                                    tickDiv(e);
+                                    tickDiv(target);
                                 } else {
                                     alert("You cannot select a seat which leaves a single seat gap.");
                                 }
-
                             }
-                        } else {
-                            if (checkNext(nextDiv, nextTwoDiv)) {
-                                tickDiv(e);
-                            } else {
-                                alert("You cannot select a seat which leaves a single seat gap.");
-                            }
-                        }
-                    } else if (target.hasClass("selected")) {
-                        if (nextDiv.hasClass("empty") || previousDiv.hasClass("empty")) {
-                            target.attr("class", "empty");
-                        } else if (nextDiv.hasClass("selected") && !previousDiv.hasClass("selected")) {
-                            target.attr("class", "empty");
-                        } else if (previousDiv.hasClass("selected") && !nextDiv.hasClass("selected")) {
-                            target.attr("class", "empty");
-                        } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
-                            if (totalCol === middleNextCol) {
+                        } else if (target.hasClass("selected")) {
+                            if (nextDiv.hasClass("empty") || previousDiv.hasClass("empty")) {
                                 target.attr("class", "empty");
-                            } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                            } else if (nextDiv.hasClass("selected") && !previousDiv.hasClass("selected")) {
                                 target.attr("class", "empty");
-                            } else {
-                                alert("You cannot un-select a seat which leaves a single seat gap.");
-                            }
-                        } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
-                            if (totalCol === middlePreviousCol) {
+                            } else if (previousDiv.hasClass("selected") && !nextDiv.hasClass("selected")) {
                                 target.attr("class", "empty");
                             } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
-                                target.attr("class", "empty");
+                                if (totalCol === middleNextCol) {
+                                    target.attr("class", "empty");
+                                } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                                    target.attr("class", "empty");
+                                } else {
+                                    alert("You cannot un-select a seat which leaves a single seat gap.");
+                                }
+                            } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                                if (totalCol === middlePreviousCol) {
+                                    target.attr("class", "empty");
+                                } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
+                                    target.attr("class", "empty");
+                                } else {
+                                    alert("You cannot un-select a seat which leaves a single seat gap.");
+                                }
                             } else {
                                 alert("You cannot un-select a seat which leaves a single seat gap.");
                             }
-                        } else {
-                            alert("You cannot un-select a seat which leaves a single seat gap.");
                         }
                     }
                 }
 
-                function initSeatsPlan(matrix) {
-                    var i, j, parent, seatTable, seatRow, seatColumn;
-                    parent = $("body");
-                    seatTable = $('<table/>', {
-                        "class": "seatTable",
-                        "id": "section1"
-                    }).appendTo(parent);
+                $(document).click(function (e) {
+                    var target, str, endOfRow, endOfCol, colValue, rowValue, totalCol, previousCol, nextCol, middlePreviousCol, middleNextCol, previousDiv, previousTwoDiv, nextDiv, nextTwoDiv;
+                    target = $(e.target);
+                    if (target.is("table.seatTable tbody tr td div")) {
+                        str = target.attr("id");
+                        endOfRow = str.indexOf("row");
+                        endOfCol = str.indexOf("col") + 3;
+                        colValue = parseInt(str.substring(endOfCol, str.length), 10);
+                        rowValue = parseInt(str.substring(0, endOfRow), 10);
 
-                    for (i = 0; i < matrix.row; i = i + 1) {
-                        seatRow = $('<tr/>', {
-                            "id": "row" + i
-                        }).appendTo(seatTable);
+                        totalCol = parseInt(seatsMatrix.col, 10);
+                        previousCol = parseInt(colValue - 2, 10);
+                        nextCol = parseInt(colValue + 2, 10);
 
-                        for (j = 0; j < matrix.col; j = j + 1) {
-                            seatColumn = $('<td/>').appendTo(seatRow);
+                        middlePreviousCol = parseInt(colValue - 1, 10);
+                        middleNextCol = parseInt(colValue + 1, 10);
 
-                            $('<div/>', {
-                                "id": i + "row" + "col" + j,
-                                "class": "empty"
-                            }).on("click", detectEmptySeat).appendTo(seatColumn);
+                        previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
+                        previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
+
+                        nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
+                        nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
+
+                        if (target.hasClass("empty")) {
+                            if (previousDiv.hasClass("empty")) {
+
+                                if (previousTwoDiv.hasClass("selected")) {
+                                    alert("You cannot select a seat which leaves a single seat gap.");
+                                } else {
+                                    if (checkNext(nextDiv, nextTwoDiv)) {
+                                        tickDiv(target);
+                                    } else {
+                                        alert("You cannot select a seat which leaves a single seat gap.");
+                                    }
+                                }
+                            } else {
+                                if (checkNext(nextDiv, nextTwoDiv)) {
+                                    tickDiv(target);
+                                } else {
+                                    alert("You cannot select a seat which leaves a single seat gap.");
+                                }
+                            }
+                        } else if (target.hasClass("selected")) {
+                            if (nextDiv.hasClass("empty") || previousDiv.hasClass("empty")) {
+                                target.attr("class", "empty");
+                            } else if (nextDiv.hasClass("selected") && !previousDiv.hasClass("selected")) {
+                                target.attr("class", "empty");
+                            } else if (previousDiv.hasClass("selected") && !nextDiv.hasClass("selected")) {
+                                target.attr("class", "empty");
+                            } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
+                                if (totalCol === middleNextCol) {
+                                    target.attr("class", "empty");
+                                } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                                    target.attr("class", "empty");
+                                } else {
+                                    alert("You cannot un-select a seat which leaves a single seat gap.");
+                                }
+                            } else if (nextDiv.hasClass("sold") || nextDiv.hasClass("locked")) {
+                                if (totalCol === middlePreviousCol || middlePreviousCol < 0) {
+                                    target.attr("class", "empty");
+                                } else if (previousDiv.hasClass("sold") || previousDiv.hasClass("locked")) {
+                                    target.attr("class", "empty");
+                                } else {
+                                    alert("You cannot un-select a seat which leaves a single seat gap.");
+                                }
+                            } else {
+                                alert("You cannot un-select a seat which leaves a single seat gap.");
+                            }
                         }
                     }
-                }
+
+                    if ($("table.seatTable tbody tr td div.selected").length > 0) {
+                        $("input[type='submit']").removeAttr("disabled");
+                        $("input#resetButton").removeAttr("disabled");
+                    } else {
+                        $("input[type='submit']").attr("disabled", "disabled");
+                        $("input#resetButton").attr("disabled", "disabled");
+                    }
+
+                });
+
                 $("#resetButton").click(function () {
                     $("table.seatTable tbody tr td div.selected").each(function () {
                         $(this).attr("class", "empty");
                     });
+                    $("input[type='submit']").attr("disabled", "disabled");
+                    $("input#resetButton").attr("disabled", "disabled");
                 });
 
-                $(function () {
-                    initSeatsPlan(seatsMatrix);
+                $("form").submit(function (e) {
+                    var form, selectedSeats, seatFlag;
+                    form = $("form");
+                    selectedSeats = $("table.seatTable tbody tr td div.selected");
+                    seatFlag = true;
+
+                    if (selectedSeats.length > 0) {
+                        $.each(selectedSeats, function (index, target) {
+                            var str, endOfRow, endOfCol, colValue, rowValue, previousCol, nextCol, middlePreviousCol, middleNextCol, previousDiv, previousTwoDiv, nextDiv, nextTwoDiv;
+                            str = $(target).attr("id");
+                            endOfRow = str.indexOf("row");
+                            endOfCol = str.indexOf("col") + 3;
+                            colValue = parseInt(str.substring(endOfCol, str.length), 10);
+                            rowValue = parseInt(str.substring(0, endOfRow), 10);
+
+                            previousCol = parseInt(colValue - 2, 10);
+                            nextCol = parseInt(colValue + 2, 10);
+
+                            middlePreviousCol = parseInt(colValue - 1, 10);
+                            middleNextCol = parseInt(colValue + 1, 10);
+
+                            previousDiv = $("#" + rowValue + "rowcol" + middlePreviousCol);
+                            previousTwoDiv = $("#" + rowValue + "rowcol" + previousCol);
+
+                            nextDiv = $("#" + rowValue + "rowcol" + middleNextCol);
+                            nextTwoDiv = $("#" + rowValue + "rowcol" + nextCol);
+
+                            if (previousDiv.hasClass("empty") && previousTwoDiv.hasClass("selected")) {
+                                seatFlag = false;
+                                return;
+                            }
+
+                            if (nextDiv.hasClass("empty") && nextTwoDiv.hasClass("selected")) {
+                                seatFlag = false;
+                                return;
+                            }
+                        });
+
+                        if (seatFlag) {
+                            form.attr("method", "post");
+                        } else {
+                            alert("You cannot select a seat which leaves a single seat gap.");
+                            return false;
+                        }
+                    }
                 });
             }
 
