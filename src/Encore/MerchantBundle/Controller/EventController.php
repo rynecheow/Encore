643fa1f1e65a16cf1a30802f35760ea6fde14ca4
James <sysadm@sysadms-iMac-4.local>
2013-11-09 20:06:07 +0800

    [update] add function complete TODO: edit function and event publish variable

--- src/Encore/MerchantBundle/Controller/EventController.php
+++ src/Encore/MerchantBundle/Controller/EventController.php
@@ -8,11 +8,14 @@
 
 namespace Encore\MerchantBundle\Controller;
 
+use Encore\CustomerBundle\Entity\EventSeat;
+use Encore\CustomerBundle\Entity\EventSection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Encore\CustomerBundle\Entity\Event;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
 
 class EventController extends BaseController
 {
@@ -32,16 +35,51 @@ class EventController extends BaseController
     {
         $allVenueLocations = $this->em->getRepository("EncoreCustomerBundle:Venue")
                                   ->findAllLocation();
-        $venuesInfo = $this->getAllVenueInfo();
         $newEvent = new Event();
-        $createEventForm = $this->createEventForm($newEvent, $venuesInfo, $allVenueLocations);
+        $createEventForm = $this->createEventForm($newEvent, $allVenueLocations);
         $createEventForm->handleRequest($request);
 
         if ($createEventForm->isValid())
         {
+            /**
+             * @var $selectedVenue \Encore\CustomerBundle\Entity\Venue
+             */
+            $selectedVenueId = $createEventForm->get("venue")->getData();
+            $selectedVenue = $this->em->getRepository("EncoreCustomerBundle:Venue")
+                                  ->find($selectedVenueId);
+            $newEvent->setVenue($selectedVenue);
             $this->em->persist($newEvent);
             $this->em->flush();
             $this->pushFlashMessage("Success", "Event has been created");
+
+            $sections = $newEvent->getVenue()->getSections();
+
+            /**
+             * @var $section \Encore\CustomerBundle\Entity\Section
+             */
+            foreach ($sections as $section)
+            {
+                $seats = $section->getSeats();
+                $eventSectionPrice = $createEventForm->get($section->getName())->getData();
+                $eventSection = new EventSection();
+                $eventSection->setEvent($newEvent)
+                             ->setSection($section)
+                             ->setPrice($eventSectionPrice)
+                             ->setTotalSeats(count($seats))
+                             ->setTotalSold(0);
+                $this->em->persist($eventSection);
+                $this->em->flush();
+
+                foreach ($seats as $seat)
+                {
+                    $eventSeat = new EventSeat();
+                    $eventSeat->setSeat($seat)
+                              ->setStatus(0)
+                              ->setEventSection($eventSection);
+                    $this->em->persist($eventSeat);
+                    $this->em->flush();
+                }
+            }
 
             return $this->render("EncoreMerchantBundle::Events:index.html.twig");
         }
@@ -58,9 +96,8 @@ class EventController extends BaseController
     {
         $allVenueLocations = $this->em->getRepository("EncoreCustomerBundle:Venue")
             ->findAllLocation();
-        $venuesInfo = $this->getAllVenueInfo();
         $request = $this->getRequest();
-        $editEventForm = $this->createEventForm($event, $venuesInfo, $allVenueLocations);
+        $editEventForm = $this->createEventForm($event, $allVenueLocations);
         $editEventForm->handleRequest($request);
 
         if ($editEventForm->isValid())
@@ -72,8 +109,19 @@ class EventController extends BaseController
                 ->setSaleStart($params["event_sale_start"])
                 ->setSaleEnd($params["event_sale_end"])
                 ->setHeldDates($params["event_held_dates"])
-                ->setTotalTickets($params["event_total_tickets"])
-                ->setVenue($params["event_venue"]);
+                ->setTotalTickets($params["event_total_tickets"]);
+
+            /**
+             * @var $selectedVenue \Encore\CustomerBundle\Entity\Venue
+             */
+            $selectedVenueId = $editEventForm->get("venue")->getData();
+
+            if ($selectedVenueId != $event->getVenue()->getId())
+            {
+                $selectedVenue = $this->em->getRepository("EncoreCustomerBundle:Venue")
+                    ->find($selectedVenueId);
+                $event->setVenue($selectedVenue);
+            }
 
             return $this->render("EncoreMerchantBundle::Events:index.html.twig");
         }
@@ -81,46 +129,65 @@ class EventController extends BaseController
         return $this->render("EncoreMerchantBundle::Events:edit-event.html.twig");
     }
 
-    private function getAllVenueInfo()
+    public function getLocationVenue()
     {
-        $allVenue = $this->em->getRepository("EncoreCustomerBundle:Venue")
-            ->findAll();
+        $request = $this->getRequest("request");
+        $location = $request->query->get("location");
+        $venues = $this->em->getRepository("EncoreCustomerBundle:Venue")
+                       ->findByLocation($location);
         $venuesInfo = [];
-        $i = 0;
 
         /**
          * @var $venue \Encore\CustomerBundle\Entity\Venue
          */
-        foreach ($allVenue as $venue)
+        foreach ($venues as $venue)
         {
-            $sections = $this->em->getRepository("EncoreCustomerBundle:Section")
-                ->findByVenue($venue);
-            $sectionsInfo = [];
-            $j = 0;
-
-            /**
-             * @var $section \Encore\CustomerBundle\Entity\Section
-             */
-            foreach ($sections as $section)
-            {
-                $sectionsInfo[$j] = [
-                    "id" => $section->getId(),
-                    "name" => $section->getName()
-                ];
-            }
-
-            $venuesInfo[$i] = [
+            $venuesInfo[] = [
                 "id" => $venue->getId(),
                 "name" => $venue->getName(),
-                "location" => $venue->getLocation(),
-                "sections" => $sectionsInfo
             ];
         }
 
-        return $venuesInfo;
+        $response = [
+            "code" => "200",
+            "status" => true,
+            "venues" => $venuesInfo
+        ];
+
+        return new Response(json_encode($response));
     }
 
-    private function createEventForm(Event $event, $venues, $allVenueLocation)
+    public function getVenueSection()
+    {
+        $request = $this->getRequest("request");
+        $venueId = $request->query->get("venueId");
+        $venue = $this->em->getRepository("EncoreCustomerBundle:Venue")
+                      ->find($venueId);
+        $sections = $venue->getSections();
+        $sectionsInfo = [];
+
+        /**
+         * @var $section \Encore\CustomerBundle\Entity\Section
+         */
+        foreach ($sections as $section)
+        {
+            $sectionsInfo[] = [
+                "id" => $section->getId(),
+                "name" => $section->getName(),
+                "total_seats" => count($section->getSeats())
+            ];
+        }
+
+        $response = [
+            "code" => "200",
+            "status" => true,
+            "sections" => $sectionsInfo
+        ];
+
+        return new Response(json_encode($response));
+    }
+
+    private function createEventForm(Event $event, $allVenueLocation)
     {
         // TODO: create form for event.
     }
