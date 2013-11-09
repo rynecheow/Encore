56229c4739ac9aec06a65e01337ac1e74cf72bdd
James <sysadm@sysadms-iMac-4.local>
2013-11-09 12:35:04 +0800

    [update] complete eventPhotoController wait for form to test

--- src/Encore/MerchantBundle/Controller/EventPhotoController.php
+++ src/Encore/MerchantBundle/Controller/EventPhotoController.php
@@ -19,58 +19,78 @@ class EventPhotoController extends BaseController
      */
     public function addPhotosAction(Event $event)
     {
+        $photos = [];
         $request = $this->getRequest();
-        $photos = $request->files->get("photos");
+        $controlPhotoForm = $this->controlPhotoForm($photos);
+        $controlPhotoForm->handleRequest($request);
 
-        if ($photos)
-        {
-            foreach ($photos as $photo)
-            {
+        if ($controlPhotoForm->isValid()) {
+            $photos = $controlPhotoForm->getData();
+
+            foreach ($photos as $photo) {
                 $eventPhoto = new EventPhoto();
                 $eventPhoto->setEvent($event)
-                           ->setImage($photo);
+                    ->setImage($photo);
                 $this->em->persist($eventPhoto);
                 $this->em->flush();
                 $event->addPhoto($eventPhoto);
             }
+
+            return $this->render("EncoreMerchantBundle::Events:index.html.twig");
         }
 
-        return [
-            "status" => true
-        ];
+        return $this->render("EncoreMerchantBundle::Events:add-event-photo.html.twig");
     }
 
     public function editPhotosAction(Event $event)
     {
+        $photos = [];
+        $eventPhotos = $event->getPhotos();
+
+        /**
+         * @var $eventPhoto \Encore\CustomerBundle\Entity\EventPhoto
+         */
+
+        foreach ($eventPhotos as $eventPhoto) {
+            $photos[] = $eventPhoto->getImage();
+        }
+
         $request = $this->getRequest();
-        $photos = $request->files->get("photos");
+        $controlPhotoForm = $this->controlPhotoForm($photos);
+        $controlPhotoForm->handleRequest($request);
 
+        if ($controlPhotoForm->isValid()) {
+            $photos = $controlPhotoForm->getData();
 
-        if ($photos)
-        {
-            foreach ($photos as $photo)
-            {
+            foreach ($photos as $photo) {
                 $eventPhoto = $this->em->getRepository("EncoreCustomerBundle:EventPhoto")
-                     ->findByImage($photo);
+                    ->findByImage($photo);
                 $exist = count($eventPhoto);
 
-                if ($exist == 0)
-                {
+                if ($exist == 0) {
                     $eventPhoto = new EventPhoto();
                     $eventPhoto->setEvent($event)
-                               ->setImage($photo);
+                        ->setImage($photo);
                     $this->em->persist($eventPhoto);
                     $this->em->flush();
                     $event->addPhoto($eventPhoto);
                 }
             }
+
+            return $this->render("EncoreMerchantBundle::Events:index.html.twig");
         }
+
+        return $this->render("EncoreMerchantBundle::Events:edit-event-photo.html.twig");
     }
 
+    /**
+     * @param $photo \Doctrine\Common\Collections\Collection
+     * @return array
+     */
     public function deletePhotoAction($photo)
     {
         $eventPhoto = $this->em->getRepository("EncoreCustomerBundle:EventPhoto")
-                           ->findByImage($photo);
+            ->findByImage($photo);
         $event = $eventPhoto->getEvent();
         $event->removePhoto($eventPhoto);
         $this->em->flush();
