b910564b91231efff1bdabe2f185c7209873a77c
Kok Hong <kokhong200@gmail.com>
2013-11-12 00:19:47 +0800

    [update] search page done with search bar

--- src/Encore/CustomerBundle/Resources/public/js/encore/search/advance-search.js
+++ src/Encore/CustomerBundle/Resources/public/js/encore/search/advance-search.js
@@ -4,6 +4,16 @@ require(['domReady'],
             function () {
                 $(function () {
                         $('input.datepicker').datepicker();
+                        $('#search a').click(function (e) {
+                            e.preventDefault();
+                            
+                            $(this).tab('show');
+                        });
+
+                        $('#filter a').click(function (e) {
+                            e.preventDefault();
+                            $(this).tab('show');
+                        });
                     }
                 );
 
