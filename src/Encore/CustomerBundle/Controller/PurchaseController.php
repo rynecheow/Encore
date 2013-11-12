9730db15d7998de478f26fb048dd94ea4bae0246
Bryan Lai <bryanlai@mystudentpartners.com>
2013-11-13 01:33:41 +0800

    [update] dynamic seat selection using ajax

--- src/Encore/CustomerBundle/Controller/PurchaseController.php
+++ src/Encore/CustomerBundle/Controller/PurchaseController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Encore\CustomerBundle\Entity\Event;
+use Symfony\Component\HttpFoundation\Response;
 
 class PurchaseController extends BaseController
 {
@@ -90,16 +91,76 @@ class PurchaseController extends BaseController
      */
     public function selectSeatAction(Event $event)
     {
+        $eventHolders = $event->getEventHolders();
+        $heldDates = [];
+        foreach ($eventHolders as $eventHolder) {
+            $heldDates[] = $eventHolder->getHeldDate()->format("Y-m-d");
+        }
+
+        sort($heldDates);
+
         $params = [
-            "event_name" => "This Event",
-            "dateArray" => ["11/12/2013", "11/13/2013", "11/15/2013", "11/17/2013"],
-            "timeArray" => ["8:00", "12:00", "14:00", "16:00", "18:00"],
-            "sectionArray" => ["VVIP", "VIP", "Premium", "Standard"],
-            "availableSeats" => 10,
-            "rowArray" => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            "totalCol" => 70
+            "event_id" => $event->getId(),
+            "event_name" => $event->getName(),
+            "dateArray" => $heldDates
         ];
 
         return $this->render("EncoreCustomerBundle:Events:seat-selection.html.twig", $params);
+    }
+
+    /**
+     * @Route("/select_date", name="encore_select_date")
+     * @Method("POST")
+     */
+    public function selectDateAction()
+    {
+
+        $id = $this->getRequest()->get('id');
+        $selectedDate = $this->getRequest()->get('date');
+        if (isset($id) && isset($selectedDate)) {
+            $result = $this->getEventHeldTime($id, $selectedDate);
+        }
+
+        if ($result) {
+            $response = [
+                "code" => $result["status"] === "error" ? 400 : 200,
+                "status" => $result["status"] === "error" ? false : true,
+                "event_times" => $result["event_times"]
+            ];
+        }
+        
+        return new Response(json_encode($response));
+    }
+
+    private function getEventHeldTime($id, $selectedDate)
+    {
+        $status = 'success';
+        $msg = 'OK';
+        /**
+         * @var $eventRepo \Encore\CustomerBundle\Repository\EventHolderRepository
+         */
+        $eventRepo = $this->em->getRepository('EncoreCustomerBundle:EventHolder');
+        $eventTimes = $eventRepo->findAllEventTimeByEventId($id);
+        $times = [];
+        if (!count($eventTimes)) {
+            $status = 'error';
+            $msg = 'There isn\'t any time available for this event';
+        } else {
+            foreach ($eventTimes as $time) {
+                $time = $time['heldDate']->format("Y-m-d H:i:s");
+                if (strpos($time, $selectedDate) !== false) {
+                    $str = date("H:i", strtotime($time));
+                    $times[] = $str;
+                }
+            }
+        }
+
+        $result = [
+            'status' => $status,
+            'message' => $msg,
+            'event_times' => $times
+        ];
+
+        return $result;
     }
 }
